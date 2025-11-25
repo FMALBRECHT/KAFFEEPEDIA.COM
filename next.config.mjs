979de03517859e32e5e50fdf8b562fce2f048067
FMALBRECHT <faizanmustafa225@gmail.com>
2025-11-24 23:00:38 -0500

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ["ts", "tsx", "mdx"],
-  // If you later add image domains or rewrite proxies, add them here.
-};
-
-export default nextConfig;
